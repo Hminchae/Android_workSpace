8a51e67c3bc1ab45e90641e2a0a5d990f7e7dd03
Hminchae <hminchae@gmail.com>
2024-07-16 14:51:29 +0900

    Add : EditText 와 버튼 액션 연결

--- firstApp/app/src/main/java/com/example/firstapp/MainActivity.java
+++ firstApp/app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.firstapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_id;
+    Button btn_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        et_id = findViewById(R.id.et_id); /*id 에 있는 값을 et_id에 부여*/
+        btn_test = findViewById(R.id.btn_test);
+
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*이제 여기서 버튼 클릭시 이벤트를 정의 해주면 된다.*/
+                et_id.setText("안녕하세용!!");
+            }
         });
     }
 }
