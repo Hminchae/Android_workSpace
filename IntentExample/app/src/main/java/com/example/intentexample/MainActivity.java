f0e3bbcbe6e2f3d5f390117eef656e7ea1799894
Hminchae <hminchae@gmail.com>
2024-07-18 00:22:49 +0900

    Add : 값전달 구현

--- IntentExample/app/src/main/java/com/example/intentexample/MainActivity.java
+++ IntentExample/app/src/main/java/com/example/intentexample/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.intentexample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_move;
+    private EditText edit_text;
+    private String str;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edit_text = findViewById(R.id.edit_text);
+        //str = edit_text.getText().toString(); // ⬅️이거 이렇게하면 앱을 첫 실행했을 때 가져오고 말아버려..
+
+        btn_move = findViewById(R.id.btn_move); /*버튼으로 id를 찾고*/
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { /*액션을 연결해준다.*/
+                str = edit_text.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SubActivity.class); /*현재 Activity 와 이동할 Activity*/
+                intent.putExtra("str", str);
+                startActivity(intent); // 액티비티 이동
+            }
         });
     }
 }
